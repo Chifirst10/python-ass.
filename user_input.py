0a41f81834f225c869a1db7687979b5e573236e1
Chifirst10 <ezeakunwannefrancis@gmail.com>
2024-11-09 15:56:10 +0100

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Please enter your name:")
+age = input("Please enter your age:")
+location = input("Pleae enter your location:")
+
+print(f"Hello {name},you are {age} years old and lives in {location}.")
